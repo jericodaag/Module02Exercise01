819b22d94ecf48786bcec95709426c00c1f5ba3c
jiaorem <yabut.micohjomarie@auf.edu.ph>
2024-08-28 13:50:30 +0800

    Add project files.

---
+++ View/EmployeePage.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.Maui.Controls;
+
+namespace Module02Exercise01.View
+{
+    public partial class EmployeePage : ContentPage
+    {
+        public EmployeePage()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
